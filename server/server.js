352adddf0febb965b92c8e4b7e2c749f02389bae
supriya2820 <supriya.10619011722@ipu.ac.in>
2025-06-28 18:41:52 +0530

    Debug: remove DB and Clerk for Vercel test

--- server/server.js
+++ server/server.js
@@ -1,27 +1,54 @@
-import express from "express"
-import "dotenv/config";
+// import express from "express"
+// import "dotenv/config";
+// import cors from "cors";
+// import connectDB from "./configs/db.js";
+// import { clerkMiddleware } from '@clerk/express'
+// import clerkWebhooks from "./controllers/clerkWebhooks.js";
+// import userRouter from "./routes/userRoutes.js";
+
+// connectDB()
+
+// const app = express()
+// app.use(cors()) //ENABLE CROSS-ORIGIN RESOURCE SHARING
+
+// //MIDDLEWARE
+// app.use(express.json())
+// app.use(clerkMiddleware())
+
+// //API TO LISTEN TO CLERK WEBHOOKS
+// app.use("/api/clerk",clerkWebhooks);
+
+// app.get('/',(req,res)=> res.send('API is Working'))
+// app.use('/api/user', userRouter)
+
+// const PORT = process.env.PORT || 3000;
+
+// // app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+// export default app; // Don't start server manually; let Vercel handle it
+
+
+
+import express from "express";
 import cors from "cors";
-import connectDB from "./configs/db.js";
-import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from "./controllers/clerkWebhooks.js";
-import userRouter from "./routes/userRoutes.js";
 
-connectDB()
+const app = express();
 
-const app = express()
-app.use(cors()) //ENABLE CROSS-ORIGIN RESOURCE SHARING
+app.use(cors());
+app.use(express.json());
 
-//MIDDLEWARE
-app.use(express.json())
-app.use(clerkMiddleware())
+// TEMP: Don't connect DB or use Clerk
+// import connectDB from "./configs/db.js";
+// connectDB();
 
-//API TO LISTEN TO CLERK WEBHOOKS
-app.use("/api/clerk",clerkWebhooks);
+// import { clerkMiddleware } from '@clerk/express';
+// app.use(clerkMiddleware());
 
-app.get('/',(req,res)=> res.send('API is Working'))
-app.use('/api/user', userRouter)
+// TEMP: Skip Clerk and DB routes
+// import clerkWebhooks from "./controllers/clerkWebhooks.js";
+// import userRouter from "./routes/userRoutes.js";
+// app.use("/api/clerk", clerkWebhooks);
+// app.use("/api/user", userRouter);
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => res.send("API is Working"));
 
-// app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
-export default app; // Don't start server manually; let Vercel handle it
+export default app;
